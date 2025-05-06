ef96b11305c08f2194966dac92277d4d6ec5c1f3
HandLoong <397706388@qq.com>
2025-05-06 16:44:18 +0800

    --

---
+++ MethodTimerExercise/MethodTimeLogger.cs
@@ -0,0 +1,25 @@
+﻿using NLogExercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    /// <summary>
+    /// 运行耗时为long（毫秒）
+    /// </summary>
+    public static class MethodTimeLogger
+    {
+        public static void Log(MethodBase methodBase, long milliseconds, string message)
+        {
+            if (milliseconds > 200)
+            {
+                MyLog.Trace("TimeMethodMonitor", $"方法：{methodBase.ReflectedType?.FullName}.{methodBase.Name} 耗时：{milliseconds} 毫秒，信息：{message}");
+            }
+            //Console.WriteLine($"方法：{methodBase.Name} 耗时：{milliseconds} 毫秒，信息：{message}");
+        }
+    }
+}
--- MethodTimerExercise/Program.cs
+++ MethodTimerExercise/Program.cs
@@ -1,10 +1,8 @@
 ﻿using MethodTimer;
 using NLogExercise;
-using System.Reflection;
 
 namespace MethodTimerExercise
 {
-    [Time]
     internal class Program
     {
         static void Main(string[] args)
@@ -14,6 +12,7 @@ namespace MethodTimerExercise
             TimeMethod(200);
         }
 
+        [Time]
         public static void TimeMethod(int total)
         {
             for (int i = 0; i < total; i++)
@@ -22,32 +21,5 @@ namespace MethodTimerExercise
                 Thread.Sleep(1);
             }
         }
-
-        /// <summary>
-        /// 运行耗时为long（毫秒）
-        /// </summary>
-        public static class MethodTimeLogger
-        {
-            public static void Log(MethodBase methodBase, long milliseconds, string message)
-            {
-                if (milliseconds > 200)
-                {
-                    MyLog.Trace("TimeMethodMonitor", $"方法：{methodBase.ReflectedType?.FullName}.{methodBase.Name} 耗时：{milliseconds} 毫秒，信息：{message}");
-                }
-                //Console.WriteLine($"方法：{methodBase.Name} 耗时：{milliseconds} 毫秒，信息：{message}");
-            }
-        }
-
-        ///// <summary>
-        ///// 运行耗时为TimeSpan
-        ///// </summary>
-        //public static class MethodTimeLogger
-        //{
-        //    public static void Log(MethodBase methodBase, TimeSpan elapsed, string message)
-        //    {
-        //        MyLog.Trace("TimeMethodMonitor", $"方法：{methodBase.ReflectedType?.FullName}.{methodBase.Name} 耗时：{elapsed.TotalMilliseconds} 毫秒，信息：{message}");
-        //        //Console.WriteLine($"方法：{methodBase.Name} 耗时：{elapsed.TotalMilliseconds} 毫秒，信息：{message}");
-        //    }
-        //}
     }
 }
