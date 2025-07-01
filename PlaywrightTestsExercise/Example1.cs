b33de49325958a2fc530a406564925b8f40a6eae
HandLoong <397706388@qq.com>
2025-07-01 18:19:24 +0800

    t_comp_gzip

---
+++ PlaywrightTestsExercise/Example1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Playwright;
+using System.Threading.Tasks;
+
+
+public class Example1
+{
+    public async Task Execute()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        var chromium = playwright.Chromium;
+        // Make sure to run headed.
+        var browser = await chromium.LaunchAsync(new() { Headless = false });
+
+        // Setup context however you like.
+        var context = await browser.NewContextAsync(); // Pass any options
+        await context.RouteAsync("**/*", route => route.ContinueAsync());
+
+        // Pause the page, and start recording manually.
+        var page = await context.NewPageAsync();
+
+        //await page.GotoAsync("http://baidu.com");
+        await page.GotoAsync("http://10.61.131.106/login", new PageGotoOptions
+        {
+            WaitUntil = WaitUntilState.DOMContentLoaded
+        });
+
+
+        //await page.FillAsync("//*[@id=\"kw\"]", "你好");
+        //await page.ClickAsync("//*[@id=\"su\"]");
+
+
+        await page.FillAsync("//*[@id=\"form_item_account\"]", "superAdmin",new PageFillOptions { 
+        
+        });
+
+        await page.FillAsync("//*[@id=\"form_item_password\"]", "P@ssw0rddzz");
+
+        await page.ClickAsync("//button[@class='ant-btn ant-btn-primary ant-btn-lg login-button w-full']");
+        await page.PauseAsync();
+    }
+}
+
